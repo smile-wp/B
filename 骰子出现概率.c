be6c10f3b0f32cf8ad4604b2f290ed85ff73bd33
smile-wp <72619680+smile-wp@users.noreply.github.com>
2020-12-06 00:53:46 +0800

    Update 骰子出现概率.c

--- 骰子出现概率.c
+++ 骰子出现概率.c
@@ -1,34 +1,17 @@
-#include<iostream>
-#include<string>
-#include<string.h>
-using namespace std;
+#include<stdio.h>
 int main()
 {
-    string a,b;
-    cin>>a>>b;
-    int la=a.length(),lb=b.length();
-    if(la<lb)
+    int i,a[6]={0},k;
+    float p;
+    for(i=0;i<6000;i++)
     {
-        for(int i=1;i=lb-la;i++)
-            a='0'+a;
+        k=rand()%6+1;
+       a[k-1]++;
+
     }
-    else
+    for(i=0;i<6;i++)
     {
-        for(int i=1;i<la-lb;i++)
-            b='0'+b;
+    p=a[i]/6000.0;
+    printf("%d   出现了%d次，出现的概率：%.2f\n",i+1,a[i],p);
     }
-    la=a.length();
-    string str;
-    int jw=0;
-    int temp;
-    for(int i=la-1;i>=0;i--)
-    {
-        temp=a[i]-'0'+b[i]-'0'+jw;
-        jw=temp/10;
-        temp%=10;
-        str=char(temp+'0')+str;
-    }
-    if(jw!=0)
-        str=char(jw+'0')+str;
-    cout<<str;
 }
